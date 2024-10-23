3a062a816a4bd3c03fd72238af293f4ca7798133
Nicky Nicolson <nicky.nicolson@gmail.com>
2024-10-23 08:47:34 +0100

    read gbif auth details from env

--- families2gbifdownloadquery.py
+++ families2gbifdownloadquery.py
@@ -5,7 +5,7 @@ import json
 from pygbif import occurrences as occ
 import os
 
-def convert_families_to_gbif_download_query(inputfile, outputfile, creator, notification_address):
+def convert_families_to_gbif_download_query(inputfile, outputfile):
     
     df = pd.read_csv(inputfile)
     
@@ -22,9 +22,12 @@ def convert_families_to_gbif_download_query(inputfile, outputfile, creator, noti
     print(json.dumps(occ_sample, indent=4))
 
     # Use pygbif to download
+    gbif_username = os.getenv('GBIF_USERNAME')
     gbif_password = os.getenv('GBIF_PASSWORD')
+    gbif_email = os.getenv('GBIF_EMAIL')
 
-    dl = occ.download(download_request_predicate, user=creator, pwd=gbif_password, email=notification_address)
+
+    dl = occ.download(download_request_predicate, user=gbif_username, pwd=gbif_password, email=gbif_email)
     download_id = dl[0]
 
     with open(outputfile, 'w') as dl_id_file:
@@ -34,10 +37,10 @@ def convert_families_to_gbif_download_query(inputfile, outputfile, creator, noti
     # GBIF endpoint using CURL.
     # This download_request_wrapper object adds the necessary information 
     # around the predicate that we defined above 
-    
+
     # download_request_wrapper = dict()
-    # download_request_wrapper['creator'] = creator
-    # download_request_wrapper['notification_address'] = [notification_address]
+    # download_request_wrapper['creator'] = gbif_username
+    # download_request_wrapper['notification_address'] = [gbif_email]
     # download_request_wrapper['sendNotification'] = True
     # download_request_wrapper['format'] = 'SIMPLE_CSV'
     # download_request_wrapper['predicate'] = download_request_predicate
@@ -49,8 +52,6 @@ def main():
     # Set up argument parser
     parser = argparse.ArgumentParser(description="Process an CSV file of GBIF-mediated specimen occurrences to request a GBIF download.")
     parser.add_argument('input_file', help="Path to the input CSV file")
-    parser.add_argument('--gbif_user_name', required=False, help="GBIF user name")
-    parser.add_argument('--gbif_email_address', required=False, help="GBIF notification email address")
     parser.add_argument('output_file', help="Path to the output download ID file")
     
     # Parse arguments
